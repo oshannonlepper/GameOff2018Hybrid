ec232d7b4ce5a5e5ab3f41cacec6031039a90ded
oshannonlepper <oshannonlepper@gmail.com>
2018-11-11 00:05:00 +0000

    Added main Game State Machine.

---
+++ Assets/Scripts/Game/GameState/GameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Primary state machine for handling the overall game flow. */
+public class GameStateManager : MonoBehaviour
+{
+	private StateMachine _gameStateMachine;
+
+	private void Awake()
+	{
+		_gameStateMachine = new StateMachine();
+		_gameStateMachine.RegisterState("MainMenu", new MainMenuGameState());
+		_gameStateMachine.RegisterState("Overworld", new OverworldGameState());
+		_gameStateMachine.RegisterState("Battle", new BattleGameState());
+		_gameStateMachine.RegisterState("BattleResult", new BattleResultGameState());
+
+		_gameStateMachine.RequestState("MainMenu");
+	}
+
+	private void Update()
+	{
+		float deltaTime = Time.deltaTime;
+		_gameStateMachine.UpdateStateMachine(deltaTime);
+	}
+}
---
+++ Assets/Scripts/Game/GameState/GameStates.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : State
+{
+	public override void OnStateEnter()
+	{
+	}
+
+	public override void OnStateExit()
+	{
+	}
+
+	public override void UpdateState(float deltaTime)
+	{
+	}
+}
+
+public class MainMenuGameState : GameState
+{
+	public override void OnStateEnter()
+	{
+		base.OnStateEnter();
+
+		Debug.Log("Press Space to start the game.");
+	}
+
+	public override void UpdateState(float deltaTime)
+	{
+		base.UpdateState(deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Owner.RequestState("Overworld");
+		}
+	}
+}
+
+public class OverworldGameState : GameState
+{
+	private string[] Locations = { "in a forest.", "in some woods...", "in a marsh.", "out on the street...", "in... a jungle?", "on the beach!", "out in the middle of the ocean...", "in a desert!?" };
+
+	public override void OnStateEnter()
+	{
+		base.OnStateEnter();
+
+		int LocationIndex = Random.Range(0, Locations.Length - 1);
+		Debug.Log("You find yourself " + Locations[LocationIndex]);
+		Debug.Log("If you hang around long enough, I'm sure some animal will attack you...");
+	}
+
+	public override void UpdateState(float deltaTime)
+	{
+		base.UpdateState(deltaTime);
+
+		if (Random.Range(0.0f, 1.0f) > 0.995f)
+		{
+			Debug.Log("Oh boy here we go!");
+			Owner.RequestState("Battle");
+		}
+	}
+}
+
+public class BattleGameState : GameState
+{
+	private Battle _battle;
+
+	public BattleGameState()
+	{
+		_battle = new Battle();
+	}
+
+	public override void OnStateEnter()
+	{
+		base.OnStateEnter();
+
+		_battle.BeginEncounter();
+	}
+
+	public override void UpdateState(float deltaTime)
+	{
+		base.UpdateState(deltaTime);
+		_battle.UpdateBattle(deltaTime);
+	}
+}
+
+public class BattleResultGameState : GameState
+{
+
+}
